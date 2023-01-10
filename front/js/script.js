dbb62ebfc254cf12968dcbf1be614227ac5917e2
David Cardon <da.cardon@outlook.fr>
2023-01-10 10:46:02 +0100

    add datas on page

--- front/js/script.js
+++ front/js/script.js
@@ -17,3 +17,29 @@ fetch(url)
     }
 )
 
+// datas on page
+
+function showProducts(products) {
+    for(let product of products) {
+
+        let allProducts = document.createElement('a');
+        document.querySelector('.items').appendChild(allProducts);
+        allProducts.href = `./product.html?id=${product._id}`;
+
+        let articleProduct = document.createElement('article');
+        allProducts.appendChild(articleProduct);
+
+        let imageProduct = document.createElement('img');
+        articleProduct.appendChild(imageProduct);
+        imageProduct.src = `${product.imageUrl}`;
+        imageProduct.alt = `${product.altTxt}`;
+
+        let nameProduct = document.createElement('h3');
+        articleProduct.appendChild(nameProduct);
+        nameProduct.innerHTML = `${product.name}`;
+
+        let descriptionProduct = document.createElement('p');
+        articleProduct.appendChild(descriptionProduct);
+        descriptionProduct.innerHTML = `${product.description}`;
+    }
+}
