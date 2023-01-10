49dbc2c2388fa93ee054bbd45411f86a0eab31b7
David Cardon <da.cardon@outlook.fr>
2023-01-10 10:49:15 +0100

    add fetch product.js

---
+++ front/js/product.js
@@ -0,0 +1,18 @@
+// Datas Url from API
+const url = 'http://localhost:3000/api/products';
+
+// All product from API
+
+fetch(url)
+    .then(response => {
+        if(response.ok){
+            response.json().then(datas => {
+                console.log(datas);
+                showProducts(datas);
+            })
+        }
+        else {
+            console.log("Une erreur s'est produite");
+        }
+    }
+)
