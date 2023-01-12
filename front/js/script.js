afc23743009c0bff6c343267647989e3a21fab06
David Cardon <da.cardon@outlook.fr>
2023-01-12 22:16:36 +0100

    fix error function allProducts

--- front/js/script.js
+++ front/js/script.js
@@ -1,41 +1,30 @@
-// Datas Url from API
-const url = "http://localhost:3000/api/products";
+// get all products from api
 
-// All product from API
-
-fetch(url).then((response) => {
-   if (response.ok) {
-      response.json().then((datas) => {
+function getProducts() {
+   fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((datas) => {
          console.log(datas);
-         showProducts(datas);
+         allProducts(datas);
+      })
+      .catch((error) => {
+         console.log("Une erreur est survenue : " + error);
       });
-   } else {
-      console.log("Une erreur s'est produite");
-   }
-});
+}
+getProducts();
 
-// datas on page
-
-function showProducts(products) {
-   for (let product of products) {
-      let allProducts = document.createElement("a");
-      document.querySelector(".items").appendChild(allProducts);
-      allProducts.href = `./product.html?id=${product._id}`;
-
-      let articleProduct = document.createElement("article");
-      allProducts.appendChild(articleProduct);
-
-      let imageProduct = document.createElement("img");
-      articleProduct.appendChild(imageProduct);
-      imageProduct.src = `${product.imageUrl}`;
-      imageProduct.alt = `${product.altTxt}`;
-
-      let nameProduct = document.createElement("h3");
-      articleProduct.appendChild(nameProduct);
-      nameProduct.innerHTML = `${product.name}`;
-
-      let descriptionProduct = document.createElement("p");
-      articleProduct.appendChild(descriptionProduct);
-      descriptionProduct.innerHTML = `${product.description}`;
+function allProducts(datas) {
+   for (let i = 0; i < datas.length; i++) {
+      let data = datas[i];
+      console.log(i);
+      let a = document.createElement("a");
+      a.setAttribute("href", `./product.html?id=${data._id}`);
+      document.querySelector("#items").appendChild(a);
+      a.innerHTML = `
+      <article>
+         <img src="${data.imageUrl}" alt="${data.altTxt}, ${data.name}">
+         <h3 class="productName">${data.name}</h3>
+         <p class="productDescription">${data.description}</p>
+      </article></a>`;
    }
 }
