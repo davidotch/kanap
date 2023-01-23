e8c7d7f5c9e28f9316e2333a8894cc05c858ac3d
David Cardon <da.cardon@outlook.fr>
2023-01-23 16:42:08 +0100

    fix function getTotalQuantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,8 +1,10 @@
-// GET BASKET DATAS FROM LOCAL STORAGE
+// ----Get datas from local stotage----
+
 let basket = JSON.parse(localStorage.getItem("localProduct"));
 console.log(basket);
 
-// GET PRODUCT DATAS FROM API BY ID TO GET ITS PRICE
+// ----Get product datas from API/ID to get price----
+
 function getProductDatas(idProduct) {
    response = fetch("http://localhost:3000/api/products/" + idProduct).then(
       (data) => {
@@ -12,8 +14,9 @@ function getProductDatas(idProduct) {
    return response;
 }
 
-// DISPLAY BASKET DATAS
-getBasket = async () => {
+// ----Basket datas----
+
+function getBasket() {
    if (basket === null || basket == 0) {
       let h1 = document.querySelector("h1");
       h1.innerText = "Votre panier est vide";
@@ -21,7 +24,7 @@ getBasket = async () => {
       for (let i = 0; i < basket.length; i++) {
          let item = basket[i];
 
-         productData = await getProductDatas(item.id);
+         productData = getProductDatas(item.id);
          console.log(productData);
 
          let article = document.createElement("article");
@@ -49,17 +52,17 @@ getBasket = async () => {
                               </div>
                             </div>`;
       }
-      getTotalQty();
+      getTotalQuantity();
       changeQty();
       removeItem();
    }
-};
+}
 
 getBasket();
 
-// GET BASKET PRODUCT(S) TOTAL QUANTITY AND TOTAL PRICE
+// ----Get basket product total qty and total price----
 
-getTotalQty = async () => {
+function getTotalQuantity() {
    const qty = document.querySelectorAll(".itemQuantity");
    let totalQty = 0;
    for (let i = 0; i < qty.length; i++) {
@@ -73,72 +76,9 @@ getTotalQty = async () => {
    for (let i = 0; i < basket.length; i++) {
       let item = basket[i];
 
-      productData = await getProductDatas(item.id);
+      productData = getProductDatas(item.id);
 
       totalPrice += qty[i].value * productData.price;
    }
    document.querySelector("#totalPrice").innerHTML = totalPrice;
-};
-
-// MODIFY A PRODUCT QUANTITY, AND REMOVE FROM BASKET AND DISPLAY WHEN EQUAL TO ZERO
-
-changeQty = () => {
-   const inputQty = document.querySelectorAll(".itemQuantity");
-
-   for (let i = 0; i < inputQty.length; i++) {
-      inputQty[i].addEventListener("change", (e) => {
-         e.preventDefault();
-
-         let modifiedValue = inputQty[i].value;
-
-         if (modifiedValue > 0 && modifiedValue <= 100) {
-            basket[i].quantity = modifiedValue;
-
-            localStorage.setItem("localProduct", JSON.stringify(basket));
-         } else if (modifiedValue > 100 || modifiedValue < 0) {
-            alert("La quantité saisie est incorrecte");
-         } else {
-            if (
-               confirm("Voulez-vous supprimer cet article du panier ?") == true
-            ) {
-               let itemToRemoveId = basket[i].id;
-               let itemToRemoveColor = basket[i].color;
-
-               newBasket = basket.filter(
-                  (e) =>
-                     e.id !== itemToRemoveId || e.color !== itemToRemoveColor
-               );
-
-               localStorage.setItem("localProduct", JSON.stringify(newBasket));
-            }
-         }
-         getTotalQty();
-      });
-   }
-};
-
-// REMOVE ITEM FROM BASKET
-removeItem = () => {
-   const removeBtn = document.querySelectorAll(".deleteItem");
-
-   for (let i = 0; i < removeBtn.length; i++) {
-      removeBtn[i].addEventListener("click", (e) => {
-         e.preventDefault();
-
-         if (
-            confirm("Voulez-vous supprimer cet article du panier ? ") == true
-         ) {
-            let itemToRemoveId = basket[i].id;
-            let itemToRemoveColor = basket[i].color;
-
-            newBasket = basket.filter(
-               (e) => e.id !== itemToRemoveId || e.color !== itemToRemoveColor
-            );
-
-            localStorage.setItem("localProduct", JSON.stringify(newBasket));
-
-            window.location.reload();
-         }
-      });
-   }
-};
+}
