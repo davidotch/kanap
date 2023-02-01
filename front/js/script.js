1149948d490666e52c1cae8637111d47bee264dc
David Cardon <da.cardon@outlook.fr>
2023-02-01 18:03:12 +0100

    Add comment

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,4 @@
-// get all products from api
-
+//----Appel de l'API pour retrouver les différents produits----
 function getProducts() {
    fetch("http://localhost:3000/api/products")
       .then((response) => response.json())
@@ -13,10 +12,13 @@ function getProducts() {
 }
 getProducts();
 
+//----Nous utilisons les informations récupérées via l'API pour afficher tous les produits----
 function allProducts(datas) {
+   //--for ([initialisation]; [condition]; [expression-finale])--
    for (let i = 0; i < datas.length; i++) {
       let data = datas[i];
       console.log(i);
+      //--Création de l'élément "a" pour afficher les articles--
       let a = document.createElement("a");
       a.setAttribute("href", `./product.html?id=${data._id}`);
       document.querySelector("#items").appendChild(a);
