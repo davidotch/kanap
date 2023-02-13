11f59ae18cf2cb25fbb5a43642dce8a63ed79d6e
David Cardon <da.cardon@outlook.fr>
2023-02-13 15:13:29 +0100

    fix nameCityRegexp final

--- front/js/cart.js
+++ front/js/cart.js
@@ -169,7 +169,7 @@ const addressError = address.nextElementSibling;
 const cityError = city.nextElementSibling;
 const emailError = email.nextElementSibling;
 
-const nameCityRegExp = /^[a-zA-Z0-9àâäéèêëïîôöùûüç ,.'-]+$/;
+const nameCityRegExp = /^[a-zA-Zàâäéèêëïîôöùûüç ,.'-]+$/;
 const addressRegExp = /^[0-9]{1,3}[a-zA-Zàâäéèêëïîôöùûüç ,.'-]+$/;
 const emailRegExp = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
 
